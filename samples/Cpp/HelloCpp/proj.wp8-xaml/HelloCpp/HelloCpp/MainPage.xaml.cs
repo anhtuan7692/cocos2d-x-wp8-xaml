dae5f981c6d52dfe6aa33b4b116f1d4e17c3b2b4
anhtuan7692 <anhtuan7692@gmail.com>
2013-12-17 13:10:29 +0700

    uplen
    len nao

---
+++ samples/Cpp/HelloCpp/proj.wp8-xaml/HelloCpp/HelloCpp/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using PhoneDirect3DXamlAppComponent;
+using Windows.Graphics.Display;
+
+namespace PhoneDirect3DXamlAppInterop
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private Direct3DBackground m_d3dBackground = null;
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawingSurfaceBackground_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (m_d3dBackground == null)
+            {
+                m_d3dBackground = new Direct3DBackground();
+
+                // Hook-up native component to DrawingSurfaceBackgroundGrid
+                DrawingSurfaceBackground.SetBackgroundContentProvider(m_d3dBackground.CreateContentProvider());
+                DrawingSurfaceBackground.SetBackgroundManipulationHandler(m_d3dBackground);
+
+                // set the desired screen orientation
+                m_d3dBackground.WindowOrientation = DisplayOrientations.Landscape;
+            }
+        }
+    }
+}
